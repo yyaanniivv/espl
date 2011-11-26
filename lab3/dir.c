cf4df6f4781134cf2689d0357ce94aecb03b3a0d
yaniv <yanivm+github@gmail.com>
2011-11-26 18:15:10 +0200

    lab5 pre

--- lab3/dir.c
+++ lab3/dir.c
@@ -2,10 +2,14 @@
 #include <dirent.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 int main (int argc,char *argv[])
 {
   int opt, hflag=0, lflag=0;
+  char *path;
   while ((opt=getopt(argc, argv, "hl")) !=-1)
   {
     switch(opt)
@@ -25,18 +29,41 @@ int main (int argc,char *argv[])
   if (!hflag)
   {
     
-    if (optind < argc)      //read dir from argv//
+    if (optind < argc) //receive dir from argv
     {
-      printf("directory: %s\n", argv[optind]);
+      //printf("directory: %s\n", argv[optind]);
+      path=argv[optind];
     }
-    else //read current location dir
+    else //read current location as dir
     {
-       printf("%s\n",getcwd(NULL,0));
+       //printf("%s\n",getcwd(NULL,0));
+       path=getcwd(NULL,0);
     }
+    //go though dirctory 
+    DIR *dip;
+    struct dirent *dit;
+
+    if( (dip=opendir(path)) != NULL)
+    {
+	//go over dir for each entry print it.
+	//if l flag is up - print more details.
+      struct stat * filestats;
+      while( (dit=readdir(dip)) !=NULL) //segmentation fault on flag -l
+      {  
+	if (lflag)
+	{
+	  stat(dit->d_name, filestats); //segmentation fault on flag -l //NOTICE
+	  printf("bytes: %lu ", filestats->st_size);
+	}
+	printf(" filename %s\n",dit->d_name );
+      }
+    }
+    else // hflag
+      printf("error opening directory %s \n",argv[optind]);
   }
   else
   {
-    printf("\tOPTIONS\n \t\t  -h   print summary of options and exit.\n 	\t\t  -l   print file size in bytes before the file name.\n");
+    printf("OPTIONS\n\t-h   print summary of options and exit.\n\t-l   print file size in bytes before the file name.\n");
   }
   return 0;
-}
+} 
